ebb279f3afc1662f6c6529f7465aeef7c9cf0fbf
Igor Jelic <igor.jelic.98@gmail.com>
2022-03-01 22:22:11 +0100

    progress

---
+++ KolibaWebApplication/WebApplication/Controllers/APIs/UsersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication.Models;
+using WebApplication.Models.DataBase;
+
+namespace WebApplication.Controllers.APIs
+{
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        [Route("api/salesmans")]
+        public IHttpActionResult GetAllSalesmans()
+        {
+            IList<Salesman> salesmans = null;
+
+            using (var db = new AppDbContext())
+            {
+                salesmans = db.Salesmans.ToList<Salesman>();
+            }
+
+            if (salesmans.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(salesmans);
+        }
+
+        [HttpPost]
+        [Route("api/register")]
+        public IHttpActionResult RegisterUser(User user)
+        {
+            if (!ValidateUser(user))
+            {
+                return BadRequest("Invalid data..");
+            }
+
+            using(var db = new AppDbContext())
+            {
+                if (db.Users.Any(u => u.Username.Equals(user.Username)))
+                {
+                    return BadRequest("Username already exists..");
+                }
+
+                //if (db.Users.Where(u => u.Username.Equals(user.Username)).ToList().Count > 0)
+                //{
+                //    return BadRequest("Username already exists..");
+                //}
+
+                db.Users.Add(new Models.User()
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Username = user.Username,
+                    Password = user.Password,
+                    Role = Models.Enums.Role.User
+                });
+
+                db.SaveChanges();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("api/registerSalesman")]
+        public IHttpActionResult RegisterSalesman(User user)
+        {
+            if (!ValidateUser(user))
+            {
+                return BadRequest("Invalid data..");
+            }
+
+            using (var db = new AppDbContext())
+            {
+                db.Salesmans.Add(new Models.Salesman()
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Username = user.Username,
+                    Password = user.Password,
+                    Role = Models.Enums.Role.Salesman
+                });
+
+                db.SaveChanges();
+            }
+
+            return Ok(user);
+        }
+
+        private bool ValidateUser(User u)
+        {
+            if (string.IsNullOrWhiteSpace(u.FirstName))
+            {
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(u.LastName))
+            {
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(u.Username))
+            {
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(u.Password))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
--- KolibaWebApplication/WebApplication/Controllers/HomeController.cs
+++ KolibaWebApplication/WebApplication/Controllers/HomeController.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Models.DataBase;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private AppDbContext _context;
+
+        public HomeController()
+        {
+            _context = new AppDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            ViewBag.Title = "Sign In Page";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Title = "Register Page";
 
             return View();
         }
