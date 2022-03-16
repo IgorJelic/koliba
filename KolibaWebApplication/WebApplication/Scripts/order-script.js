059ccfb4dca8c0109bcfbe7e99d56b0482b26995
Igor Jelic <igor.jelic.98@gmail.com>
2022-03-16 16:14:17 +0100

    order details working

--- KolibaWebApplication/WebApplication/Scripts/order-script.js
+++ KolibaWebApplication/WebApplication/Scripts/order-script.js
@@ -13,11 +13,8 @@
             }
 
             $.get(url);
-            //$.get(url, (result) => {
-            //    alert(result);
-            //})
+
             window.location.href = "../Home/Index";
-            //alert("Uspesno kreirana narudzbina");
         }
         else {
             return
@@ -30,16 +27,48 @@
 
     $(".btn-order-details").click((e) => {
         const id = $(e.currentTarget).data('idval');
+        const sumprice = $(e.currentTarget).data('sumprice');
 
         var getUrl = "http://localhost:49693/api/orders/" + id;
 
         $.get(getUrl, (data, status) => {
-            alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-        })
-    })
+            const orderId = $("#order-id");
+            const meals = $("#meals");
+            const drinks = $("#drinks");
+            const price = $("#price");
 
-    $("#myBtn").click(() => {
-        $(".modal").css("display", "block");
+            let orderIdData = data.Id;
+            let drinksData = data.OrderedDrinks;
+            let mealsData = data.OrderedMeals;
+
+            meals.empty();
+            drinks.empty();
+
+            orderId.text("Order ID: " + orderIdData);
+
+            mealsData.forEach((meal) => {
+                let p = document.createElement("p");
+                p.classList.add("meal");
+
+                //p.innerText = "Burek(400g)";
+                p.innerText = `${meal.Name} - ${meal.Quantity}g`;
+
+                meals.append(p);
+            })
+
+            drinksData.forEach((drink) => {
+                let p = document.createElement("p");
+                p.classList.add("drink");
+
+                p.innerText = `${drink.Name} - ${drink.Quantity}kom`;
+
+                drinks.append(p);
+            })
+
+            price.text("Cena: " + sumprice + "din");
+
+            $(".modal").css("display", "block");
+        })
     })
 
     $(".close").click(() => {
